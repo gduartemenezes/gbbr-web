be88350fd85c6be35c7e5284ad41347104969eab
Gustavo Duarte <gduartemenezes@gmail.com>
2020-06-29 14:43:57 -0300

    Add handleSubmit method to auth pages

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+
 import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="Go Barber" />
 
-      <form action="">
-        <input type="email" name="email" placeholder="Seu e-mail" />
-        <input
+      <Form onSubmit={handleSubmit}>
+        <Input type="email" name="email" placeholder="Seu e-mail" />
+        <Input
           type="password"
           name="password"
           placeholder="Sua senha secreta"
         />
         <button type="submit">Acessar</button>
         <Link to="/register">Criar conta gratuita</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+
 import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
   return (
     <>
       <img src={logo} alt="Go Barber" />
 
-      <form action="">
-        <input type="text" name="name" placeholder="Nome completo" />
-        <input type="email" name="email" placeholder="Seu e-mail" />
-        <input
+      <Form onSubmit={handleSubmit}>
+        <Input type="text" name="name" placeholder="Nome completo" />
+        <Input type="email" name="email" placeholder="Seu e-mail" />
+        <Input
           type="password"
           name="password"
           placeholder="Sua senha secreta"
         />
         <button type="submit">Criar conta</button>
-        <Link to="/">Voltar para Login</Link>
-      </form>
+        <Link to="/">Já tenho login</Link>
+      </Form>
     </>
   );
 }
