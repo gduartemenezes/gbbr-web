1b67539f1192794aa7dcc2a36593159adc84dc29
Gustavo Duarte <gduartemenezes@gmail.com>
2020-06-29 14:35:37 -0300

    Add content to sign up page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
   return (
-    <div>
-      <h1>sign up</h1>
-    </div>
+    <>
+      <img src={logo} alt="Go Barber" />
+
+      <form action="">
+        <input type="text" name="name" placeholder="Nome completo" />
+        <input type="email" name="email" placeholder="Seu e-mail" />
+        <input
+          type="password"
+          name="password"
+          placeholder="Sua senha secreta"
+        />
+        <button type="submit">Criar conta</button>
+        <Link to="/">Voltar para Login</Link>
+      </form>
+    </>
   );
 }
