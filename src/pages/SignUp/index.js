b9892346902c730dd8499c95ffd0ed3cecb2d7eb
Gustavo Duarte <gduartemenezes@gmail.com>
2020-06-29 14:50:25 -0300

    Add data validation to auth pages

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('Nome é um campo obrigatório'),
+  email: Yup.string()
+    .email('E-mail inválido')
+    .required('Nome é um campo obrigatório'),
+  password: Yup.string().required('Senha é um campo obrigatório'),
+});
 
 export default function SignUp() {
   function handleSubmit(data) {
@@ -12,7 +21,7 @@ export default function SignUp() {
     <>
       <img src={logo} alt="Go Barber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input type="text" name="name" placeholder="Nome completo" />
         <Input type="email" name="email" placeholder="Seu e-mail" />
         <Input
