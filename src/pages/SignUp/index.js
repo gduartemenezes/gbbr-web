1872f021e54aac4d886d7524230ef01619b73b52
Gustavo Duarte <gduartemenezes@gmail.com>
2020-06-30 09:42:01 -0300

    Add signUp action to app

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
+
+import { signUpRequest } from '~/store/modules/auth/actions';
 
 import logo from '~/assets/logo.svg';
 
@@ -16,15 +19,17 @@ const schema = Yup.object().shape({
 });
 
 export default function SignUp() {
-  function handleSubmit(data) {
-    console.tron.log(data);
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
   }
   return (
     <>
       <img src={logo} alt="Go Barber" />
 
       <Form schema={schema} onSubmit={handleSubmit}>
-        <Input type="text" name="name" placeholder="Nome completo" />
+        <Input name="name" placeholder="Nome completo" />
         <Input type="email" name="email" placeholder="Seu e-mail" />
         <Input
           type="password"
