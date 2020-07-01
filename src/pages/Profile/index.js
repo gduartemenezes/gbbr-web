b92ee8ff3f099b6e8e729b5c618b6d2ad228b30f
Gustavo Duarte <gduartemenezes@gmail.com>
2020-07-01 08:30:49 -0300

    Add content to profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
+import { Container } from './styles';
 
 export default function Profile() {
+  const profile = useSelector((state) => state.user.profile);
+
+  function handleSubmit() {}
+
   return (
-    <div>
-      <h1>profile</h1>
-    </div>
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de email" />
+        <hr />
+
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Sua senha atual"
+        />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirme sua senha"
+        />
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
   );
 }
