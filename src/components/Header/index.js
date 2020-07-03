583031abf8abc0a6bb7a459d83ab7b7c88916dd6
Gustavo Duarte <gduartemenezes@gmail.com>
2020-07-03 08:35:26 -0300

    Add profile state to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Notifications from '~/components/Notifications';
@@ -7,6 +8,8 @@ import { Container, Content, Profile } from './styles';
 import logo from '~/assets/logo-purple.svg';
 
 export default function Header() {
+  const profile = useSelector((state) => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -19,11 +22,14 @@ export default function Header() {
           <Notifications />
           <Profile>
             <div>
-              <strong>Gustavo Duarte</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/55/abott@adorable.png"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/55/abott@adorable.png'
+              }
               alt="Gustavo Duarte"
             />
           </Profile>
