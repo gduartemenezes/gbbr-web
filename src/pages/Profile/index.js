8fefd4f8f7ffb7f0ed41a5d61586892ead0e7014
Gustavo Duarte <gduartemenezes@gmail.com>
2020-07-03 08:28:47 -0300

    Add Avatar component to profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -3,7 +3,7 @@ import { Form, Input } from '@rocketseat/unform';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { updateProfileRequest } from '~/store/modules/user/actions';
-
+import AvatarInput from './AvatarInput';
 import { Container } from './styles';
 
 export default function Profile() {
@@ -17,6 +17,7 @@ export default function Profile() {
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
         <Input name="name" type="text" placeholder="Nome Completo" />
         <Input name="email" type="email" placeholder="Seu endereço de email" />
         <hr />
